f6205ab30528141bc4c90a0bdd16cf276a1923ff
Omonjon <omonjons077@gmail.com>
2023-09-14 22:18:27 +0500

    Site done

--- assets/script/main.js
+++ assets/script/main.js
@@ -7,7 +7,7 @@ window.addEventListener("load", function (eventObject) {
 
 });
 
-/*=============== Header Fixed ===============*/
+/*=============== HEADER FIXED ===============*/
 if ($("#myHeader").length) {
     window.onscroll = function () {
         myFunction()
@@ -23,7 +23,72 @@ if ($("#myHeader").length) {
             header.classList.remove("sticky");
         }
     }
+};
+
+/*=============== SWIPER | HOW WORKS ===============*/
+if ($(".howWorks").length) {
+    let swiperTestimonial = new Swiper(".howWorks", {
+        grabCursor: true,
+        loop: true,
+        autoplay: true,
+        effect: '',
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        keyboard: true,
+    });
+};
+
+/*=============== SWIPER | PARTNERS ===============*/
+if ($(".partnerSwiper").length) {
+    let swiper = new Swiper(".partnerSwiper", {
+        slidesPerView: 6,
+        spaceBetween: 30,
+        loop: true,
+        grabCursor: true,
+        autoplay: true,
+        breakpoints: {
+            // when window width is >= 280px
+            280: {
+                slidesPerView: 1,
+                // spaceBetween: 0,
+                // slideToClickedSlide: true,
+            },
+            // when window width is >= 575px
+            575: {
+                slidesPerView: 2,
+                // spaceBetween: 0,
+                // slideToClickedSlide: true,
+            },
+            // when window width is >= 768px
+            768: {
+                slidesPerView: 3,
+                // spaceBetween: 0,
+                // slideToClickedSlide: true,
+            },
+            // when window width is >= 992px
+            992: {
+                slidesPerView: 4,
+                // spaceBetween: 0,
+                // slideToClickedSlide: true,
+            },
+            // when window width is >= 1200px
+            1200: {
+                slidesPerView: 5,
+                // spaceBetween: 0,
+                // slideToClickedSlide: true,
+            },
+            // when window width is >= 1440px
+            1440: {
+                slidesPerView: 6,
+                // spaceBetween: 0,
+                // slideToClickedSlide: true,
+            },
+        }
+    });
 }
+
 /*=============== SHOW SCROLL UP ===============*/
 const scrollUp = () => {
     const scrollUp = document.getElementById('scroll-up')
